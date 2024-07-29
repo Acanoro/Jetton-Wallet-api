db9953f7766bf9884652153f83284242857de9a0
User <ntumasevic@gmail.com>
2024-07-29 20:49:41 +0300

    add CORS

--- jetton_wallet_api/config/settings.py
+++ jetton_wallet_api/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # Libraries
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
 
     # Applications
     'admin_dashboard',
@@ -54,6 +55,34 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 ROOT_URLCONF = 'config.urls'
